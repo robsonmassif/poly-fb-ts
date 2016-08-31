5dc3ae3acc061bae3c914941d401681a55e454c2
robsonmassif <robson.massif@gmail.com>
2016-08-31 15:14:13 -0700

    Add compile step to gulpfile
    compile-ts creates js for polymer components

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var tsc = require('gulp-typescript');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+var typescriptOptions = {
+  target: 'ES5',
+  //module: 'ES6',
+  declarationFiles: false,
+  noExternalResolve: true,
+  experimentalDecorators: true,
+  emitDecoratorMetadata: true,
+  noEmitOnError: true
+};
+
+
+
+gulp.task('compile-ts', function() {
+
+  var tsResult = gulp.src(['app/**/*.ts', 'typings/**/*.ts'])
+    .pipe(tsc(typescriptOptions))
+    .pipe(gulp.dest('app'));
+
 });
