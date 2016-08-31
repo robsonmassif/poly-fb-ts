98a701b54ef0ce5271399822aeb83c71a86d030d
robsonmassif <robson.massif@gmail.com>
2016-08-31 15:14:01 -0700

    Have ts do something

--- app/pixel-grid.ts
+++ app/pixel-grid.ts
@@ -1,6 +1,9 @@
 
 @component("pixel-grid")
 class PixelGrid extends polymer.Base {
+  ready() {
+    console.log("pixel-grid ready!");
+  }
 }
 
 // after the element is defined, we register it in Polymer
